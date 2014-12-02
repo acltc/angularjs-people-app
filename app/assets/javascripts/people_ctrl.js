9fa5d548b0e81d8d0c033e8f4f809a932159e8e5
Jay Wengrow <jaywngrw@gmail.com>
2014-12-02 16:32:42 -0600

    posting with angular!

--- app/assets/javascripts/people_ctrl.js
+++ app/assets/javascripts/people_ctrl.js
@@ -7,9 +7,20 @@
       $scope.people = response.data;
     });
 
+    $scope.addPerson = function(name, details) {
+      var newPerson = { name: name, details: details };
+      $http.post('/api/v1/people.json', {person: newPerson}).then(function(response) {
+      }, function (error) {
+        $scope.error = error.statusText;
+      });
+      $scope.people.push(newPerson);
+    }
+
     $scope.toggleVisible = function(person) {
       person.detailsVisible = !person.detailsVisible;
     };
+
+
   });
 
 }());
--- app/controllers/api/v1/people_controller.rb
+++ app/controllers/api/v1/people_controller.rb
@@ -2,4 +2,14 @@ class Api::V1::PeopleController < ApplicationController
   def index
     @people = Person.all
   end
+
+  def create
+    @person = Person.create(person_params)
+  end
+
+  private
+
+  def person_params
+    params.require(:person).permit(:name, :details)
+  end
 end
