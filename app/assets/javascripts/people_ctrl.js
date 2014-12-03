08341249cfe88fcb80fed36816f43d42c7d4d287
Jay Wengrow <jaywngrw@gmail.com>
2014-12-03 11:01:46 -0600

    ordering by name or details

--- app/assets/javascripts/people_ctrl.js
+++ app/assets/javascripts/people_ctrl.js
@@ -3,6 +3,14 @@
 
   angular.module("app").controller("peopleCtrl", function($scope, $http){
 
+    $scope.orderByAttribute = "name";
+    $scope.descending = true;
+
+    $scope.changeOrderAttribute = function(attribute) {
+      $scope.orderByAttribute = attribute;
+      $scope.descending = !$scope.descending;
+    };
+
     $http.get("/api/v1/people.json").then(function (response) {
       $scope.people = response.data;
     });
