763a29ee1cac10bc3305529d304dcd8876fe9ebd
Jay Wengrow <jaywngrw@gmail.com>
2015-03-30 15:51:58 -0500

    successful post request

--- app/assets/javascripts/people_ctrl.js
+++ app/assets/javascripts/people_ctrl.js
@@ -8,6 +8,16 @@
         $scope.people = response.data["people"];
       });
     };
+
+    $scope.addPerson = function(name, details) {
+      var newPerson = { name: name, details: details };
+      
+      $http.post('/api/v1/people.json', newPerson).then(function(response) {
+        $scope.people.push(newPerson);
+      }, function (error) {
+        $scope.error = error.statusText;
+      });
+    };
     
     $scope.toggleVisible = function(person) {
       person.detailsVisible = !person.detailsVisible;
