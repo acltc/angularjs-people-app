d6e23af3847574e6d34c8fac4ec10c4602c2aa50
Jay Wengrow <jaywngrw@gmail.com>
2015-04-01 18:42:51 -0500

    added post request to ajax

--- app/assets/javascripts/people_ctrl.js
+++ app/assets/javascripts/people_ctrl.js
@@ -9,7 +9,23 @@
         $scope.people = response.data["people"];
       });
     };
-    
+
+    $scope.addPerson = function(newName, newDetails) {
+      var person = {
+        name: newName,
+        details: newDetails
+      };
+
+      $http.post('/api/v1/people.json', person).then(function(response) {
+          $scope.people.push(person);
+        }, function (error) {
+
+        });
+
+
+      $scope.newPersonName = "";
+      $scope.newPersonDetails = "";
+    };
 
     $scope.toggleVisible = function(person) {
       person.detailsVisible = !person.detailsVisible;
